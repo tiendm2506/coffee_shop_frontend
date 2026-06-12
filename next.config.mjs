df762a2227bdd9ae4d35823ea6fa11fac5412466
Đào Minh Tiến <minhtien2506it@gmail.com>
2026-06-12 12:19:04 +0700

    Update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
+import { loadEnvConfig } from '@next/env'
+
+const { combinedEnv } = loadEnvConfig(process.cwd())
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: 'https://demoshop.site/api',
-    NEXT_PUBLIC_SERVER_BACKEND_URL: 'https://demoshop.site'
+    NEXT_PUBLIC_API_URL: combinedEnv.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_SERVER_BACKEND_URL: combinedEnv.NEXT_PUBLIC_SERVER_BACKEND_URL
   },
   images: {
     remotePatterns: [
