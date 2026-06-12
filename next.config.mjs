18d66be0a30cb523bacff8dce39d5018f8781d4d
Đào Minh Tiến <minhtien2506it@gmail.com>
2026-06-12 14:23:06 +0700

    Update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,27 @@
-import nextEnv from '@next/env'
-const { loadEnvConfig } = nextEnv
+import fs from 'fs'
+import path from 'path'
 
-const { combinedEnv } = loadEnvConfig(process.cwd())
+const envPath = path.resolve(process.cwd(), '.env.production')
+console.log('ENV PATH:', envPath)
+console.log('FILE EXISTS:', fs.existsSync(envPath))
+
+const envFile = fs.readFileSync(envPath, 'utf-8')
+console.log('ENV FILE CONTENT:', envFile)
+
+const env = Object.fromEntries(
+  envFile.split('\n')
+    .filter(line => line.trim() && !line.startsWith('#'))
+    .map(line => line.split('='))
+)
+
+console.log('PARSED ENV:', env)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: combinedEnv.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_SERVER_BACKEND_URL: combinedEnv.NEXT_PUBLIC_SERVER_BACKEND_URL
+    NEXT_PUBLIC_API_URL: env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_SERVER_BACKEND_URL: env.NEXT_PUBLIC_SERVER_BACKEND_URL
   },
   images: {
     remotePatterns: [
